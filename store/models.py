3077a8f7220703200e3e372043c5c1c6a99a18ab
Rishabh Tiwari <rishabh422tiwari@gmail.com>
2023-07-20 21:48:31 +0530

    reverting migration

--- store/models.py
+++ store/models.py
@@ -29,7 +29,7 @@ class Customer(models.Model):
         (MEMEBERSHIP_SILVER, 'Silver'),
         (MEMEBERSHIP_GOLD, 'Gold'),
     ]
-    first_name = models.CharField(max_length=255) 
+    given_name = models.CharField(max_length=255) 
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
@@ -39,7 +39,7 @@ class Customer(models.Model):
     class Meta:
         db_table = 'store_customers'
         indexes = [
-            models.Index(fields=['last_name', 'first_name'])
+            models.Index(fields=['last_name', 'given_name'])
         ]
 
 class Order(models.Model):
