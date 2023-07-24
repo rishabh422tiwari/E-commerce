100e68efa79833bc0872c5ba9fe393077d68c4e6
Rishabh Tiwari <rishabh422tiwari@gmail.com>
2023-07-24 23:07:13 +0530

    Admin generic relationship

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from typing import Any, List, Optional, Tuple
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models.query import QuerySet
 from django.http import HttpRequest
 from django.http.request import HttpRequest
@@ -7,6 +7,7 @@ from django.urls import reverse
 from django.utils.html import format_html, urlencode
 from django.db.models.aggregates import Count
 from . import models
+from tags.models import TaggedItem, Tag
 
 class InventoryFilter(admin.SimpleListFilter):
     title = 'inventory'
@@ -20,15 +21,23 @@ class InventoryFilter(admin.SimpleListFilter):
     def queryset(self, request, queryset):
         if self.value() == '<10':
             queryset.filter(inventory__lt=10)
+    
+
 
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    actions = ['clear_inventory']
     list_display = ['title', 'unit_price','inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
     list_select_related = ['collection']
+    search_fields = ['title']
 
     def collection_title(self, product):
         return product.collection.title
@@ -39,14 +48,20 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         return 'OK'
 
-@admin.register(models.Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'placed_at', 'customer']
+    @admin.action(description='Clear Inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were successfully updated',
+            messages.ERROR
+        )
 
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
+    search_fields = ['title']
 
     @admin.display(ordering='products_count')
     def products_count(self,collection):
@@ -86,7 +101,19 @@ class CustomerAdmin(admin.ModelAdmin):
         return super().get_queryset(request).annotate(
             orders_count= Count('order')
         )
+    
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ['product']
+    model = models.OrderItem
+    min_num = 1
+    max_num = 10
+    extra = 0
                 
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+    inlines = [OrderItemInline]
+    autocomplete_fields = ['customer']
 
 
 
---
+++ store_customer/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StoreCustomerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'store_customer'
